f30c7116977ff38155c5692928ece8e648b7ba76
akincengiz <akincengiz@outlook.com.tr>
2025-08-05 23:42:18 +0300

    Frontend initial

---
+++ src/contexts/PersonalInfoContext.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useEffect, useState } from 'react'
+
+const PersonalInfoContext = createContext();
+
+const PersonalInfoContextProvider = ({children}) => {
+    const [personalInfos,setPersonalInfos] = useState([]);
+
+    useEffect(() => {
+        const getPersonalInfos = async() => {
+            try {
+                const response = await fetch("https://localhost:7110/api/PersonalInfos");
+                const data = await response.json();
+                setPersonalInfos(data);
+            } catch (error) {
+                console.error("Kişisel bilgiler getirilemedi...",error);
+            }
+        };
+        getPersonalInfos();
+    },[])
+
+    const personalInfoValues = {
+        personalInfos,
+        setPersonalInfos
+    }
+
+  return <PersonalInfoContext.Provider value={personalInfoValues}>
+        {children}
+    </PersonalInfoContext.Provider>
+  
+}
+
+export {PersonalInfoContext, PersonalInfoContextProvider}
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { PersonalInfoContextProvider } from './contexts/PersonalInfoContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <PersonalInfoContextProvider>
+      <App />
+    </PersonalInfoContextProvider>    
+  </StrictMode>,
+)
